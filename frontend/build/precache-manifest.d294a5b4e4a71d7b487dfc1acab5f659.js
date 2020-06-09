6a23254f53c05b10be9fd0ca87c0bb3ec72ceef0
Jan Kárník <karnikjan12@gmail.com>
2020-06-09 18:50:57 +0200

    un gitignored build

---
+++ frontend/build/precache-manifest.d294a5b4e4a71d7b487dfc1acab5f659.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e80da77611612172e52c068776ef009a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd4900b4f6bd45c5ef2e",
+    "url": "/static/css/main.a55b7e7d.chunk.css"
+  },
+  {
+    "revision": "17c59b999ccf71e1b779",
+    "url": "/static/js/2.33df0e0b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.33df0e0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd4900b4f6bd45c5ef2e",
+    "url": "/static/js/main.451a0374.chunk.js"
+  },
+  {
+    "revision": "700d31d1c6f37b7bdc51",
+    "url": "/static/js/runtime-main.84578d5e.js"
+  }
+]);
