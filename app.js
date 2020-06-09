90f206d2f37c17e71547ab73d8cb7a8cce68182c
Jan Kárník <karnikjan12@gmail.com>
2020-06-09 19:00:55 +0200

    bodyparser moved up

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 app.get('/ping', (req, res) => res.send("pong"));
 
+app.use(bodyParser.json());
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
 });
 
-app.use(bodyParser.json());
 
 
 app.use((req, res, next) => {
